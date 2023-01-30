af61f4f956421293cc697be41c1159acec3d4a92
EAMBWHITE <eambwhite@gmail.com>
2023-01-29 23:50:55 -0600

    testing

--- components/_ListRol.tsx
+++ components/_ListRol.tsx
@@ -1,48 +1,63 @@
-import { useEffect, useState } from "react";
-import querySnapshot from "../pages/api/firebaseApi";
+import { useCallback, useEffect, useState } from "react";
+// import { getData } from "../pages/api/firebaseApi";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import { FechaType } from "./types/firebaseTypes.type";
+import { getLocaleDate } from "../pages/api/util";
+import { Avatar, ListItemAvatar } from "@mui/material";
+import firebaseApp from "../pages/api/clientApp";
+import { getFirestore, collection } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 export default function ListRol() {
-  const [fechas, setFechas] = useState<FechaType[]>(querySnapshot || null);
+  // const [fechas, setFechas] = useState<any[]>([]);
+  const [fechas, loading, error] = useCollection(
+    collection(getFirestore(firebaseApp), "fechas"),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  );
 
-  const getLocaleDate = (date: Date) => {
-    var options: Intl.DateTimeFormatOptions = {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
-    return date.toLocaleDateString("es-ES", options).toUpperCase();
-  };
-
-  useEffect(() => {
-    console.log(querySnapshot);
-  }, []);
+  fechas?.docs.map((item) => {
+    return (
+      // <ListItem key={item.responsables.principal}>
+      //   <ListItemText
+      //     primary={getLocaleDate(item.fecha.toDate())}
+      //     primaryTypographyProps={{
+      //       fontSize: 20,
+      //       fontWeight: "medium",
+      //       letterSpacing: 0,
+      //       color: "black",
+      //     }}
+      //     secondary={item.responsables.datos_principal?.nombre}
+      //   />
+      // </ListItem>
+      console.log(item.data())
+    );
+  });
 
   return (
+    // <>
+    //   {fechas.length > 0 ? (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      {fechas.map((fecha) => {
-        return (
-          <ListItem key={fecha.responsables.principal}>
-            <ListItemText
-              primary={getLocaleDate(fecha.fecha.toDate())}
-              secondary={fecha.responsables.principal}
-            />
-          </ListItem>
-        );
-      })}
-      <ListItem>
-        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemText primary="Work" secondary="Jan 7, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemText primary="Vacation" secondary="July 20, 2014" />
-      </ListItem>
+      <>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar></Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Work" secondary="Jan 7, 2014" />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar></Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Vacation" secondary="July 20, 2014" />
+        </ListItem>
+      </>
     </List>
+    //   ) : (
+    //     <CircularProgress color="secondary" />
+    //   )}
+    // </>
   );
 }
